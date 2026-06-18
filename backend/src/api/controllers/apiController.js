1ac5ce1ab1305f0c4591ab9a31c3e74aad1f1d71
Axel <castellanoaxl@gmail.com>
2026-06-18 19:23:40 -0300

    (extras): Implementación de encuesta al cliente

--- backend/src/api/controllers/apiController.js
+++ backend/src/api/controllers/apiController.js
@@ -60,3 +60,35 @@ export const postVenta = async (req, res) => {
         conn.release()//devuelve la conexion exclusiva del pool del conexiones de la DB
     }
 }
+
+//funcion API-Cliente para insertar en la DB la encuesta
+export const postEncuesta = async (req, res) => {
+    const { email, puntuacion, recomienda, opinion } = req.body;
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if(!email || !emailRegex.test(email)){
+        return res.status(400).json({error: "Ingrese un email válido"})
+    }//validamos el mail
+    
+    const nota = parseInt(puntuacion);
+    if(isNaN(nota) || nota < 1 || nota > 10){
+        return res.status(400).json({error: "Ingrese una puntuación entre 1 y 10"})
+    }//validamos la puntuacion dentro de los rangos
+
+    try{
+        const recomiendaNum = recomienda === "1" ? 1: 0;//convertimos la puntuacion a int
+        const archivoUrl = req.file ? `/uploads/${req.file.filename}` : null;//verificamos si subió un archivo o no
+
+        const response = await connection.query("INSERT INTO encuestas (cliente_email, opinion, recomienda, puntuacion, archivoUrl) VALUES (?, ?, ?, ?, ?)", [email, opinion, recomiendaNum, nota, archivoUrl]);
+
+        return res.status(201).json({
+            status: "success",
+            message: "¡Muchas gracias por completar la encuesta!"
+        });
+
+    }catch(error){
+        console.log(error)
+        res.status(500).json({error: error.message});
+    }
+}
