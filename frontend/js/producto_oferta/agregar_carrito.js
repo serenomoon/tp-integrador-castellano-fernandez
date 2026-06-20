bd22d0e8088d89a897c050ba4f443cc7a2315207
Axel <castellanoaxl@gmail.com>
2026-06-20 15:34:42 -0300

    correcciones de estilos para mobile

--- frontend/js/producto_oferta/agregar_carrito.js
+++ frontend/js/producto_oferta/agregar_carrito.js
@@ -5,19 +5,22 @@ function guardarCarrito(){
 }
 
 function agregarAlCarrito(producto) {
-    const productoExistente = carrito.find(prod => prod.id === producto.id);
-
-    if (productoExistente) {
-        productoExistente.cantidad++;
-    } else {
-        carrito.push({
-            ...producto,
-            cantidad: 1
-        });
-    }
-    guardarCarrito();
-    window.mostrarAlerta("Producto agregado", `${producto.name} se agregó al carrito 🛒`);
-    
-    notificarCambioCarrito();
-    
+    window.mostrarConfirmacion(
+        "Agregar al carrito", 
+        `¿Querés agregar ${producto.name} al carrito? 🛒`,
+        () => {
+            const productoExistente = carrito.find(prod => prod.id === producto.id);
+            if (productoExistente) {
+                productoExistente.cantidad++;
+            } else {
+                carrito.push({
+                    ...producto,
+                    cantidad: 1
+                });
+            }
+            guardarCarrito();
+            notificarCambioCarrito();
+            window.mostrarExito("¡Agregado!", `${producto.name} se agregó al carrito correctamente.`);
+        }
+    );
 }
